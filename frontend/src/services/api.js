5d0ea920f485794564b5298b02b632d3e54c11e2
omkarj147 <omkarj147@gmail.com>
2024-12-04 10:43:09 +0530

    Export API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = window.location.hostname === 'localhost'
+export const API_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:5002/api'
   : 'https://tiffin-new.onrender.com/api';
 
